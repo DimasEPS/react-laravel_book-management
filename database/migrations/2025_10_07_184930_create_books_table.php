c987ea2a44631bc34d048ea42ee092089dc0ead0
DimasEPS <diaskld4@gmail.com>
2025-10-08 02:07:11 +0700

    add books tables, controller & routes

--- database/migrations/2025_10_07_184930_create_books_table.php
+++ database/migrations/2025_10_07_184930_create_books_table.php
@@ -11,8 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('otps', function (Blueprint $table) {
+        Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->text('description')->nullable();
+            $table->integer('year')->nullable();
             $table->timestamps();
         });
     }
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('otps');
+        Schema::dropIfExists('books');
     }
 };
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\OtpController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/send-otp', [OtpController::class, 'sendOtp']);
 Route::post('/auth/verify-otp', [OtpController::class, 'verifyOtp']);
+
+// book routes
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('books', BookController::class);
+});
